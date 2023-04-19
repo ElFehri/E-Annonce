a7a8e0e3bccab344d5e0676d1b28e604dda9171f
ElFehri <elfihrisami98@gmail.com>
2023-04-19 01:05:59 +0000

    react begin

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//React routes......................................................
+Route::get('/api/users', 'UserController@index');
