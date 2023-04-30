ec28c521e8e8af993136f00c57bf6bb9d0974aad
ElFehri <elfihrisami98@gmail.com>
2023-04-30 23:42:35 +0100

    merge node_modules in react-app

--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -14,7 +14,7 @@ class CheckUserRole
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $user = $request->user();
 
